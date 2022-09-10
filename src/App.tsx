67d365b3e844913ec79589551a9501e17b5e0bc8
Dandi Gilang Ramadhan <dandi.gilang@wgs-id.com>
2022-09-10 22:23:28 +0700

    add: project announcement

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className="App">
+        <header className="App-header">
+          <img src="/logo512.png" className="App-logo" alt="logo" />
+          <p>
+            Welcome to Helvatia!
+          </p>
+          <p>
+            Coming soon...
+          </p>
+          <a className='App-link' href='https://github.com/mrdhanz/helvatia'>
+            Github Repository
+          </a>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            This project is powered by React
+          </a>
+        </header>
+      </div>
+    </React.Fragment>
   );
 }
 
